52bcf2739616c778eee6100b83757fb2d70d33af
Akshaisali <akshaisali007@gmail.com>
2023-05-30 09:53:24 +0530

    test random word min length

--- test_hangman.py
+++ test_hangman.py
@@ -13,3 +13,14 @@ def test_random_word_no_scpl():
         word = hangman.random_word("/tmp/words.txt")
         assert word == "elephant"
     os.unlink("/tmp/words.txt")
+
+def test_random_word_min_length():
+    with open("/tmp/words.txt", "w") as f:
+        f.write("elephant\n")
+        f.write("egg\n")
+        f.write("an\n")
+        f.write("fun\n")
+    for _ in range(100):
+        word = hangman.random_word("/tmp/words.txt")
+        assert word == "elephant"
+    os.unlink("/tmp/words.txt")
