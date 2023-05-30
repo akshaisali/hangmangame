25f56fdf5388dbb63467de869d7ee82f3c568bc0
Akshaisali <akshaisali007@gmail.com>
2023-05-30 10:43:25 +0530

    test  check already guessed

--- test_hangman.py
+++ test_hangman.py
@@ -86,3 +86,16 @@ Guesses :
 Remaining turns : 8"""
     )
 
+def test_check_already_guessed():
+    secret_word = "hospital"
+    guesses = ["i", "t"]
+    turns_remaining = 5
+    new_guess = "t"
+    status, turns_remaining = hangman.check(
+        secret_word, guesses, turns_remaining, new_guess
+    )
+    assert status == hangman.ALREADY_GUESSED
+    assert turns_remaining == 5
+    assert guesses == ["i", "t"]
+
+
